f88fbde9901217dec4d896e1f23a38d4ef082d07
Edward Kwartler <edwardkwartler@Edwards-MacBook-Air.local>
2023-08-01 12:54:09 -0400

    moved file

---
+++ codeScaffold.R
@@ -0,0 +1,35 @@
+#' Code Scaffold
+#' TK
+#' Aug 1 2023
+#' 
+
+# Where are your local files?
+folderPath <- '~/Desktop/HBS_execEDU/groupCompanyData/GroupData1/group-1'
+
+# Get the file names
+tmp <- list.files(path = _______, pattern = ____, full.names = T)
+
+# Read in a single file
+oneCompany <- _______(____[_])
+
+# Column names
+
+# Who is speaking
+
+
+# What are the titles
+
+# See top msg 
+
+
+# Grepl - you pick the terms
+
+
+# Grep - subset based on a term
+
+
+# Keyword Density
+sum(____)/nrow(_____)
+
+
+# End
